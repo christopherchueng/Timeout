116a0bd1d98e8a4b36241476b0199a4e63b3de48
christopherchueng <christopherchueng@gmail.com>
2022-07-07 12:36:05 -0500

    Created Alarmlist and Dashboard component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { authenticate } from './store/session';
+import User from './components/User';
+import UsersList from './components/UsersList';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import UsersList from './components/UsersList';
-import User from './components/User';
-import { useSelector } from 'react-redux';
-import { authenticate } from './store/session';
 import SplashPage from './components/SplashPage';
+import Dashboard from './components/Dashboard';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -42,7 +43,7 @@ function App() {
           <SignUpForm />
         </Route>
         <ProtectedRoute path='/dashboard' exact={true} >
-          <h1>My Dashboard</h1>
+          <Dashboard />
         </ProtectedRoute>
       </Switch>
     </BrowserRouter>
---
+++ react-app/src/components/Dashboard/index.js
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+        <>
+            <h1>Hi from Dashboard!!!</h1>
+        </>
+    )
+}
+
+export default Dashboard
