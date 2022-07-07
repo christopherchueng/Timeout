e9c5deda57a49ed0a0deb970b7a964042ffdc453
christopherchueng <christopherchueng@gmail.com>
2022-07-07 12:52:35 -0500

    getAlarmList frontend done and about to work on backend

--- react-app/src/components/AlarmList/index.js
+++ react-app/src/components/AlarmList/index.js
@@ -1,7 +1,15 @@
 import { useState, useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
+import { getAlarmlists } from "../../store/alarmlist"
 
 const AlarmList = () => {
+    const dispatch = useDispatch()
+    const alarmlists = useSelector(state => state?.alarmlists?.entries)
+
+    useEffect(() => {
+        dispatch(getAlarmlists())
+    }, [dispatch])
+
     return (
         <>
             <h1>Here is the alarm list!</h1>
