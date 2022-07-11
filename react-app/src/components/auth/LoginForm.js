5f3599debe23ad956efe99782247d64e382813e8
christopherchueng <christopherchueng@gmail.com>
2022-07-11 19:15:35 -0400

    Added validators to login

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,17 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
 
 const LoginForm = () => {
-  const [errors, setErrors] = useState([]);
+  const [errors, setErrors] = useState({});
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const user = useSelector(state => state.session.user);
+  const [isSubmitted, setIsSubmitted] = useState(false)
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const validationErrors = {}
+    const emailRegex = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$/i
+
+    if (!email) {
+      validationErrors['email'] = 'Please provide an email.'
+    }
+
+    if (!emailRegex.test(email)) {
+      validationErrors['email'] = 'Please enter a valid email.'
+    }
+
+    if (!password) {
+      validationErrors['password'] = 'Please provide a password.'
+    }
+
+  }, [email, password])
 
   const onLogin = async (e) => {
     e.preventDefault();
+    setIsSubmitted(true)
     const data = await dispatch(login(email, password));
     if (data) {
       setErrors(data);
@@ -32,11 +53,7 @@ const LoginForm = () => {
 
   return (
     <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
+      {/* ---------------------- EMAIL ---------------------- */}
       <div>
         <label htmlFor='email'>Email</label>
         <input
@@ -47,6 +64,11 @@ const LoginForm = () => {
           onChange={updateEmail}
         />
       </div>
+      <div className='login-emailError-ctn'>
+       {isSubmitted && <ErrorMessage error={errors.email} setClassName="login-emailError" />}
+      </div>
+
+      {/* ---------------------- PASSWORD ---------------------- */}
       <div>
         <label htmlFor='password'>Password</label>
         <input
@@ -56,6 +78,11 @@ const LoginForm = () => {
           value={password}
           onChange={updatePassword}
         />
+      </div>
+      <div className='login-pwError-ctn'>
+       {isSubmitted && <ErrorMessage error={errors.password} setClassName="login-pwError" />}
+      </div>
+      <div className='login-btn'>
         <button type='submit'>Login</button>
       </div>
     </form>
