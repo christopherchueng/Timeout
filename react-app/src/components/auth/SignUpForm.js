f416f6a5d8a21dfa2ac56d136b369011f2883e7c
christopherchueng <christopherchueng@gmail.com>
2022-07-11 10:32:56 -0400

    Changed signUpForm and added first name and last name

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -2,28 +2,37 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
 
 const SignUpForm = () => {
-  const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
+  const [errors, setErrors] = useState({});
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false)
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
   const onSignUp = async (e) => {
     e.preventDefault();
+    setIsSubmitted(true)
     if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
+      const data = await dispatch(signUp(firstName, lastName, email, password));
       if (data) {
+        setIsSubmitted(false)
         setErrors(data)
       }
     }
   };
 
-  const updateUsername = (e) => {
-    setUsername(e.target.value);
+  const updateFirstName = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const updateLastName = (e) => {
+    setLastName(e.target.value);
   };
 
   const updateEmail = (e) => {
@@ -44,20 +53,35 @@ const SignUpForm = () => {
 
   return (
     <form onSubmit={onSignUp}>
+      {/* --------------------------- FIRST NAME --------------------------- */}
       <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label>User Name</label>
+        <label>First Name</label>
         <input
           type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
+          name='firstName'
+          onChange={updateFirstName}
+          value={firstName}
         ></input>
       </div>
+      <div className='firstName-error-ctn'>
+       {isSubmitted && <ErrorMessage error={errors.name} setClassName="firstName-error" />}
+      </div>
+
+      {/* --------------------------- LAST NAME --------------------------- */}
+      <div>
+        <label>Last Name</label>
+        <input
+          type='text'
+          name='lastName'
+          onChange={updateLastName}
+          value={lastName}
+        ></input>
+      </div>
+      <div className='lastName-error-ctn'>
+       {isSubmitted && <ErrorMessage error={errors.name} setClassName="lastName-error" />}
+      </div>
+
+      {/* --------------------------- EMAIL --------------------------- */}
       <div>
         <label>Email</label>
         <input
@@ -67,6 +91,11 @@ const SignUpForm = () => {
           value={email}
         ></input>
       </div>
+      <div className='register-email-error-ctn'>
+       {isSubmitted && <ErrorMessage error={errors.name} setClassName="register-email-error" />}
+      </div>
+
+      {/* --------------------------- PASSWORD --------------------------- */}
       <div>
         <label>Password</label>
         <input
@@ -76,6 +105,11 @@ const SignUpForm = () => {
           value={password}
         ></input>
       </div>
+      <div className='register-pw-error-ctn'>
+       {isSubmitted && <ErrorMessage error={errors.name} setClassName="register-pw-error" />}
+      </div>
+
+      {/* --------------------------- REPEAT PASSWORD --------------------------- */}
       <div>
         <label>Repeat Password</label>
         <input
@@ -86,6 +120,10 @@ const SignUpForm = () => {
           required={true}
         ></input>
       </div>
+      <div className='register-pwRepeat-error-ctn'>
+       {isSubmitted && <ErrorMessage error={errors.name} setClassName="register-pwRepeat-error" />}
+      </div>
+
       <button type='submit'>Sign Up</button>
     </form>
   );
