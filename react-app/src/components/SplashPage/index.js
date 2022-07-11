12478df545eb2ea08af5e33c883f4b85c850efda
christopherchueng <christopherchueng@gmail.com>
2022-07-10 21:39:31 -0400

    Worked on the time on splash page and organization

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,19 +1,71 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import DemoUserForm from '../auth/DemoUserForm';
 import './SplashPage.css'
 
 const SplashPage = () => {
+    const currentTime = new Date()
+    const [hour, setHour] = useState((currentTime.getHours() + 24) % 12 || 12)
+    const [minutes, setMinutes] = useState(currentTime.getMinutes())
+    const [seconds, setSeconds] = useState(currentTime.getSeconds())
+    const [meridiem, setMeridiem] = useState(currentTime.getHours() >= 12 ? 'PM' : 'AM')
+
+    useEffect(() => {
+        const secInterval = setInterval(() => setSeconds(currentTime.getSeconds()), 1000)
+        const minInterval = setInterval(() => setMinutes(currentTime.getMinutes()), 1000)
+        const hourInterval = setInterval(() => setHour((currentTime.getHours() + 24) % 12 || 12), 1000)
+        const meridInterval = setInterval(() => setMeridiem(currentTime.getHours() >= 12 ? 'PM' : 'AM'), 1000)
+        return () => {
+            clearInterval(secInterval)
+            clearInterval(minInterval)
+            clearInterval(hourInterval)
+            clearInterval(meridInterval)
+        }
+    })
+
     return (
-        <>
-            <h1>Hi from Splash</h1>
-            <Link to='/login'>
-                Login
-            </Link>
-            <Link to='/register'>
-                Register
-            </Link>
-            <DemoUserForm />
-        </>
+        <div id='splash-page'>
+            {/* ---------------------------- LOGO ---------------------------- */}
+            <div className='splash-timeout-logo'>
+                <img className='splash-page-icon' src={process.env.PUBLIC_URL + '../../../../static/timeout-black.png'}></img>
+            </div>
+            {/* ---------------------------- TIME ---------------------------- */}
+            <div className='splash-full-time'>
+                <div className='splash-time-ctn'>
+                    {/* {time} */}
+                    <div className='splash-hour'>
+                        {hour}
+                    </div>
+                    <div className='splash-second-colon'>
+                        {seconds % 2 === 0 ? ":" : ""}
+                    </div>
+                    <div className='splash-minutes'>
+                        {minutes < 10 ? '0' + minutes : minutes}
+                    </div>
+                    <div className='meridiem-ctn'>
+                        <div className='splash-meridiem'>
+                            {meridiem}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* ---------------------------- ENTRIES ---------------------------- */}
+            <div className='splash-entries'>
+                <div className='splash-login'>
+                    <Link to='/login'>
+                        Login
+                    </Link>
+                </div>
+                <div className='splash-register'>
+                    <Link to='/register'>
+                        Register
+                    </Link>
+                </div>
+                <div className='splash-demo'>
+                    <DemoUserForm />
+                </div>
+            </div>
+        </div>
     )
 }
 
