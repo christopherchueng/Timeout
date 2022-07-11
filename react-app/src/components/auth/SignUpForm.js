3d05f2881f53e0278d017c2a5572d6a98203bebf
christopherchueng <christopherchueng@gmail.com>
2022-07-11 11:18:17 -0400

    Added validations to sign up form

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
@@ -15,10 +15,34 @@ const SignUpForm = () => {
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    const validationErrors = {}
+    const emailRegex = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$/i
+
+    if (!firstName) {
+      validationErrors['firstName'] = 'Please provide a first name.'
+    }
+
+    if (!lastName) {
+      validationErrors['lastName'] = 'Please provide a last name.'
+    }
+
+    if (!emailRegex.test(email)) {
+      validationErrors['email'] = 'Please enter a valid email.'
+    }
+
+    if (password !== repeatPassword) {
+      validationErrors['repeatPassword'] = "'Password' and 'Repeat Password' fields did not match."
+    }
+
+    setErrors(validationErrors)
+
+  }, [firstName, lastName, email, repeatPassword])
+
   const onSignUp = async (e) => {
     e.preventDefault();
     setIsSubmitted(true)
-    if (password === repeatPassword) {
+    if (password === repeatPassword && errors.length === 0) {
       const data = await dispatch(signUp(firstName, lastName, email, password));
       if (data) {
         setIsSubmitted(false)
@@ -48,7 +72,7 @@ const SignUpForm = () => {
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to='/dashboard' />;
   }
 
   return (
@@ -64,7 +88,7 @@ const SignUpForm = () => {
         ></input>
       </div>
       <div className='firstName-error-ctn'>
-       {isSubmitted && <ErrorMessage error={errors.name} setClassName="firstName-error" />}
+       {isSubmitted && <ErrorMessage error={errors.firstName} setClassName="firstName-error" />}
       </div>
 
       {/* --------------------------- LAST NAME --------------------------- */}
@@ -78,7 +102,7 @@ const SignUpForm = () => {
         ></input>
       </div>
       <div className='lastName-error-ctn'>
-       {isSubmitted && <ErrorMessage error={errors.name} setClassName="lastName-error" />}
+       {isSubmitted && <ErrorMessage error={errors.lastName} setClassName="lastName-error" />}
       </div>
 
       {/* --------------------------- EMAIL --------------------------- */}
@@ -92,7 +116,7 @@ const SignUpForm = () => {
         ></input>
       </div>
       <div className='register-email-error-ctn'>
-       {isSubmitted && <ErrorMessage error={errors.name} setClassName="register-email-error" />}
+       {isSubmitted && <ErrorMessage error={errors.email} setClassName="register-email-error" />}
       </div>
 
       {/* --------------------------- PASSWORD --------------------------- */}
@@ -104,9 +128,6 @@ const SignUpForm = () => {
           onChange={updatePassword}
           value={password}
         ></input>
-      </div>
-      <div className='register-pw-error-ctn'>
-       {isSubmitted && <ErrorMessage error={errors.name} setClassName="register-pw-error" />}
       </div>
 
       {/* --------------------------- REPEAT PASSWORD --------------------------- */}
@@ -121,10 +142,10 @@ const SignUpForm = () => {
         ></input>
       </div>
       <div className='register-pwRepeat-error-ctn'>
-       {isSubmitted && <ErrorMessage error={errors.name} setClassName="register-pwRepeat-error" />}
+       {isSubmitted && <ErrorMessage error={errors.repeatPassword} setClassName="register-pwRepeat-error" />}
       </div>
 
-      <button type='submit'>Sign Up</button>
+      <button type='submit' disabled={errors.length}>Sign Up</button>
     </form>
   );
 };
