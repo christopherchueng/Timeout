6b5846fdadb8b4add9d80cde1049d65d8d661abf
christopherchueng <christopherchueng@gmail.com>
2022-07-11 23:12:44 -0400

    Styled login page

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import './LoginForm.css'
 
 const LoginForm = () => {
   const [errors, setErrors] = useState({});
@@ -52,40 +53,48 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      {/* ---------------------- EMAIL ---------------------- */}
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div className='login-emailError-ctn'>
-       {isSubmitted && <ErrorMessage error={errors.email} setClassName="login-emailError" />}
-      </div>
+    <div id='login'>
+      <form onSubmit={onLogin} className='login-form'>
+        {/* ---------------------- EMAIL ---------------------- */}
+        <div className='login-email-ctn'>
+          {/* <label htmlFor='email'>Email</label> */}
+          <input
+            name='email'
+            type='text'
+            placeholder='Email'
+            value={email}
+            onChange={updateEmail}
+            className='email-input'
+          />
+        </div>
+        <div className='login-emailError-ctn'>
+         {isSubmitted && <ErrorMessage error={errors.email} setClassName="login-emailError" />}
+        </div>
 
-      {/* ---------------------- PASSWORD ---------------------- */}
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-      </div>
-      <div className='login-pwError-ctn'>
-       {isSubmitted && <ErrorMessage error={errors.password} setClassName="login-pwError" />}
-      </div>
-      <div className='login-btn'>
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+        {/* ---------------------- PASSWORD ---------------------- */}
+        <div className='login-password-ctn'>
+          {/* <label htmlFor='password'>Password</label> */}
+          <input
+            name='password'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+            className='password-input'
+          />
+        </div>
+        <div className='login-pwError-ctn'>
+         {isSubmitted && <ErrorMessage error={errors.password} setClassName="login-pwError" />}
+        </div>
+        <div className='login-btn'>
+          <button type='submit' className='toggle-login'>
+            <span className='login-span'>Login</span>
+            <div className='ball'>
+            </div>
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
