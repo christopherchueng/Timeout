9270546fd4e59b0aeea0cc5379fa7d89795b0013
christopherchueng <christopherchueng@gmail.com>
2022-07-14 19:31:15 -0400

    Added global time to dashboard

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -2,6 +2,7 @@ import React, { useEffect} from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getAlarmlists } from "../../store/alarmlist"
 import { getAlarms } from "../../store/alarm"
+import { useTimeContext } from "../../context/TimeContext"
 import CreateAlarmlistModal from "../AlarmList/CreateAlarmlistModal"
 import Alarm from "../Alarm"
 import './Dashboard.css'
@@ -12,6 +13,7 @@ const Dashboard = () => {
     const alarmlistsObj = useSelector(state => state?.alarmlist?.entries)
     const alarmlistsArr = Object.values(alarmlistsObj)
     const alarmlists = alarmlistsArr.reverse()
+    const { hour, minutes, seconds, meridiem, currentTime } = useTimeContext()
 
     useEffect(() => {
         // Get all alarmlists under the current user (Backend will grab the current session user)
@@ -26,15 +28,34 @@ const Dashboard = () => {
 
     return (
         <div id='dashboard'>
-            <div className='create-alarmlist-modal'>
-                <CreateAlarmlistModal />
+            <div className='alarmlist-menu'>
+                <div className='create-alarmlist-modal'>
+                    <CreateAlarmlistModal />
+                </div>
+                <div className='alarmlist-content'>
+                    {alarmlists && alarmlists.map(alarmlist => (
+                        <div key={alarmlist.id}>
+                            <AlarmList dashAlarmlist={alarmlist} />
+                        </div>
+                    ))}
+                </div>
             </div>
-            <div className='alarmlist-content'>
-                {alarmlists && alarmlists.map(alarmlist => (
-                    <div key={alarmlist.id}>
-                        <AlarmList dashAlarmlist={alarmlist} />
+            <div className='global-dashboard-time'>
+                <div className='splash-hour'>
+                    {hour}
+                </div>
+                <div className='splash-second-colon'>
+                    {seconds % 2 === 0 ? ":" : ""}
+                    {/* {seconds} */}
+                </div>
+                <div className='splash-minutes'>
+                    {minutes < 10 ? '0' + minutes : minutes}
+                </div>
+                <div className='meridiem-ctn'>
+                    <div className='splash-meridiem'>
+                        {meridiem}
                     </div>
-                ))}
+                </div>
             </div>
         </div>
     )
