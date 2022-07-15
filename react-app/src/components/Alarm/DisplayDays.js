015035abe7eb4b190a728190b26608401c650e5f
christopherchueng <christopherchueng@gmail.com>
2022-07-15 00:00:53 -0400

    Added length conditionals to accoutn for other edge cases

--- react-app/src/components/Alarm/DisplayDays.js
+++ react-app/src/components/Alarm/DisplayDays.js
@@ -6,22 +6,22 @@ const DisplayDays = ({ alarmDays }) => {
         dayNums.push(day.id)
     }
     if (alarmDays.length === 1) return `every ${alarmDays[0].name}`
-    if (dayNums.every(day => weekdays.includes(day))) return 'every weekday'
-    if (dayNums.every(day => weekends.includes(day))) return 'every weekend'
+    if (alarmDays.length === 5 && dayNums.every(day => weekdays.includes(day))) return 'every weekday'
+    if (alarmDays.length === 2 && dayNums.every(day => weekends.includes(day))) return 'every weekend'
     if (dayNums.length === 7) return 'every day'
 
     return (
-        <div>
+        <>
             {alarmDays && alarmDays.map(day => (
-                <div key={day.id}>
+                <div className='selected-day' key={day.id}>
                     {(alarmDays.length === 1) ? `every ${day.name}` : '' ||
-                    (alarmDays.every(day => weekdays.includes(day)) ? 'every weekday' : '') ||
-                    (alarmDays.every(day => weekends.includes(day))) ? 'every weekend' : '' ||
+                    (alarmDays.length === 5 && alarmDays.every(day => weekdays.includes(day)) ? 'every weekday' : '') ||
+                    (alarmDays.length === 2 && alarmDays.every(day => weekends.includes(day))) ? 'every weekend' : '' ||
                     (alarmDays.length === 7) ? 'every day' : '' ||
                     (day.short)}
                 </div>
             ))}
-        </div>
+        </>
     )
 }
 
