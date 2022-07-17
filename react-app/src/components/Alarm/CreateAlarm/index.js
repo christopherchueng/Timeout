d9b2a5ac2c7d3d29d1b661d15b8dddb57a052c85
christopherchueng <christopherchueng@gmail.com>
2022-07-17 15:09:16 -0400

    Finished editing create and edit alarm routes

--- react-app/src/components/Alarm/CreateAlarm/index.js
+++ react-app/src/components/Alarm/CreateAlarm/index.js
@@ -113,109 +113,111 @@ const CreateAlarm = () => {
     }
 
     return (
-        <div className='create-alarm'>
-            <h1>Add Alarm</h1>
+        <div id='create-alarm'>
+            <h1 className='alarm-form-title'>Add Alarm</h1>
             <form onSubmit={onSubmit}>
-                {/* ------------------------- HOUR ------------------------- */}
-                <div className='alarm-hour-form'>
-                    <select
-                        name='hour'
-                        value={hour}
-                        onChange={e => setHour(parseInt(e.target.value))}
-                    >
-                        <option value='1'>1</option>
-                        <option value='2'>2</option>
-                        <option value='3'>3</option>
-                        <option value='4'>4</option>
-                        <option value='5'>5</option>
-                        <option value='6'>6</option>
-                        <option value='7'>7</option>
-                        <option value='8'>8</option>
-                        <option value='9'>9</option>
-                        <option value='10'>10</option>
-                        <option value='11'>11</option>
-                        <option value='12'>12</option>
-                    </select>
-                </div>
-                {/* ------------------------- MINUTES ------------------------- */}
-                <div className='alarm-minutes-form'>
-                    <select
-                        name='minutes'
-                        value={minutes}
-                        onChange={e => setMinutes(parseInt(e.target.value))}
-                    >
-                        <option value='0'>00</option>
-                        <option value='1'>01</option>
-                        <option value='2'>02</option>
-                        <option value='3'>03</option>
-                        <option value='4'>04</option>
-                        <option value='5'>05</option>
-                        <option value='6'>06</option>
-                        <option value='7'>07</option>
-                        <option value='8'>08</option>
-                        <option value='9'>09</option>
-                        <option value='10'>10</option>
-                        <option value='11'>11</option>
-                        <option value='12'>12</option>
-                        <option value='13'>13</option>
-                        <option value='14'>14</option>
-                        <option value='15'>15</option>
-                        <option value='16'>16</option>
-                        <option value='17'>17</option>
-                        <option value='18'>18</option>
-                        <option value='19'>19</option>
-                        <option value='20'>20</option>
-                        <option value='21'>21</option>
-                        <option value='22'>22</option>
-                        <option value='23'>23</option>
-                        <option value='24'>24</option>
-                        <option value='25'>25</option>
-                        <option value='26'>26</option>
-                        <option value='27'>27</option>
-                        <option value='28'>28</option>
-                        <option value='29'>29</option>
-                        <option value='30'>30</option>
-                        <option value='31'>31</option>
-                        <option value='32'>32</option>
-                        <option value='33'>33</option>
-                        <option value='34'>34</option>
-                        <option value='35'>35</option>
-                        <option value='36'>36</option>
-                        <option value='37'>37</option>
-                        <option value='38'>38</option>
-                        <option value='39'>39</option>
-                        <option value='40'>40</option>
-                        <option value='41'>41</option>
-                        <option value='42'>42</option>
-                        <option value='43'>43</option>
-                        <option value='44'>44</option>
-                        <option value='45'>45</option>
-                        <option value='46'>46</option>
-                        <option value='47'>47</option>
-                        <option value='48'>48</option>
-                        <option value='49'>49</option>
-                        <option value='50'>50</option>
-                        <option value='51'>51</option>
-                        <option value='52'>52</option>
-                        <option value='53'>53</option>
-                        <option value='54'>54</option>
-                        <option value='55'>55</option>
-                        <option value='56'>56</option>
-                        <option value='57'>57</option>
-                        <option value='58'>58</option>
-                        <option value='59'>59</option>
-                    </select>
-                </div>
-                {/* ------------------------- MERIDIEM ------------------------- */}
-                <div className='alarm-meridiem-form'>
-                    <select
-                        name='meridiem'
-                        value={meridiem}
-                        onChange={e => setMeridiem(e.target.value)}
-                    >
-                        <option value='AM'>AM</option>
-                        <option value='PM'>PM</option>
-                    </select>
+                <div className='select-times-and-meridiem'>
+                    {/* ------------------------- HOUR ------------------------- */}
+                    <div className='alarm-hour-form'>
+                        <select
+                            name='hour'
+                            value={hour}
+                            onChange={e => setHour(parseInt(e.target.value))}
+                        >
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                            <option value='3'>3</option>
+                            <option value='4'>4</option>
+                            <option value='5'>5</option>
+                            <option value='6'>6</option>
+                            <option value='7'>7</option>
+                            <option value='8'>8</option>
+                            <option value='9'>9</option>
+                            <option value='10'>10</option>
+                            <option value='11'>11</option>
+                            <option value='12'>12</option>
+                        </select>
+                    </div>
+                    {/* ------------------------- MINUTES ------------------------- */}
+                    <div className='alarm-minutes-form'>
+                        <select
+                            name='minutes'
+                            value={minutes}
+                            onChange={e => setMinutes(parseInt(e.target.value))}
+                        >
+                            <option value='0'>00</option>
+                            <option value='1'>01</option>
+                            <option value='2'>02</option>
+                            <option value='3'>03</option>
+                            <option value='4'>04</option>
+                            <option value='5'>05</option>
+                            <option value='6'>06</option>
+                            <option value='7'>07</option>
+                            <option value='8'>08</option>
+                            <option value='9'>09</option>
+                            <option value='10'>10</option>
+                            <option value='11'>11</option>
+                            <option value='12'>12</option>
+                            <option value='13'>13</option>
+                            <option value='14'>14</option>
+                            <option value='15'>15</option>
+                            <option value='16'>16</option>
+                            <option value='17'>17</option>
+                            <option value='18'>18</option>
+                            <option value='19'>19</option>
+                            <option value='20'>20</option>
+                            <option value='21'>21</option>
+                            <option value='22'>22</option>
+                            <option value='23'>23</option>
+                            <option value='24'>24</option>
+                            <option value='25'>25</option>
+                            <option value='26'>26</option>
+                            <option value='27'>27</option>
+                            <option value='28'>28</option>
+                            <option value='29'>29</option>
+                            <option value='30'>30</option>
+                            <option value='31'>31</option>
+                            <option value='32'>32</option>
+                            <option value='33'>33</option>
+                            <option value='34'>34</option>
+                            <option value='35'>35</option>
+                            <option value='36'>36</option>
+                            <option value='37'>37</option>
+                            <option value='38'>38</option>
+                            <option value='39'>39</option>
+                            <option value='40'>40</option>
+                            <option value='41'>41</option>
+                            <option value='42'>42</option>
+                            <option value='43'>43</option>
+                            <option value='44'>44</option>
+                            <option value='45'>45</option>
+                            <option value='46'>46</option>
+                            <option value='47'>47</option>
+                            <option value='48'>48</option>
+                            <option value='49'>49</option>
+                            <option value='50'>50</option>
+                            <option value='51'>51</option>
+                            <option value='52'>52</option>
+                            <option value='53'>53</option>
+                            <option value='54'>54</option>
+                            <option value='55'>55</option>
+                            <option value='56'>56</option>
+                            <option value='57'>57</option>
+                            <option value='58'>58</option>
+                            <option value='59'>59</option>
+                        </select>
+                    </div>
+                    {/* ------------------------- MERIDIEM ------------------------- */}
+                    <div className='alarm-meridiem-form'>
+                        <select
+                            name='meridiem'
+                            value={meridiem}
+                            onChange={e => setMeridiem(e.target.value)}
+                        >
+                            <option value='AM'>AM</option>
+                            <option value='PM'>PM</option>
+                        </select>
+                    </div>
                 </div>
                 {/* ------------------------- NAME ------------------------- */}
                 <div className='alarm-name-form'>
@@ -236,7 +238,7 @@ const CreateAlarm = () => {
                     <div className='name-char-count'>
                         {nameFocus
                         ?
-                        <>
+                        <div className='char-count-ctn'>
                             {messageCount > 150
                             ?   <div className='char-count-cmt' style={{color: 'red', width: '70px'}}>
                                     <span>{messageCount} / 150</span>
@@ -244,7 +246,7 @@ const CreateAlarm = () => {
                             :   <div className='char-count-cmt'>
                                     <span>{messageCount} / 150</span>
                                 </div>}
-                        </>
+                        </div>
                         : ''
                         }
                     </div>
@@ -262,6 +264,7 @@ const CreateAlarm = () => {
                             name='alarmlist'
                             value={alarmlist}
                             onChange={e => setAlarmlist(e.target.value)}
+                            className='alarmlist-selection-form'
                         >
                             {alarmlistsArr && alarmlistsArr.map(alarmlist => (
                                 <option value={parseInt(alarmlist.id)} key={parseInt(alarmlist.id)}>{alarmlist.name}</option>
@@ -313,7 +316,14 @@ const CreateAlarm = () => {
                     </div>
                 </div>
                 <div className='create-alarm-submit'>
-                    <button type='submit' disabled={Object.values(errors).length !== 0}>Save</button>
+                    <button
+                        type='submit'
+                        disabled={Object.values(errors).length !== 0}
+                        className='submit-alarmlist-switch'
+                    >
+                        <span className="submit-alarmlist-span">Save</span>
+                        <div className='submit-alarmlist-ball'></div>
+                    </button>
                 </div>
             </form>
         </div>
