c862a6a2f3e75ab9a355679bf5d872502756cc3c
christopherchueng <christopherchueng@gmail.com>
2022-10-05 22:48:38 -0400

    Refactored display days component

--- react-app/src/components/Alarm/DisplayDays.js
+++ react-app/src/components/Alarm/DisplayDays.js
@@ -1,23 +1,25 @@
 const DisplayDays = ({ alarmDays }) => {
     const weekdays = [1, 2, 3, 4, 5]
     const weekends = [0, 6]
-    let dayNums = []
-    for (let day of alarmDays) {
-        dayNums.push(day.id)
-    }
-    if (alarmDays.length === 1) return `every ${alarmDays[0].name}`
-    if (alarmDays.length === 5 && dayNums.every(day => weekdays.includes(day))) return 'every weekday'
-    if (alarmDays.length === 2 && dayNums.every(day => weekends.includes(day))) return 'every weekend'
-    if (dayNums.length === 7) return 'every day'
+
+    const oneDay = alarmDays.length === 1
+    const everyWeekday = alarmDays.every(day => weekdays.includes(day.id))
+    const everyWeekend = alarmDays.every(day => weekends.includes(day.id))
+    const everyDay = alarmDays.length === 7
+
+    if (oneDay) return `every ${alarmDays[0].name}`
+    if (everyWeekday) return 'every weekday'
+    if (everyWeekend) return 'every weekend'
+    if (everyDay) return 'every day'
 
     return (
         <>
             {alarmDays && alarmDays.map(day => (
                 <div className='selected-day' key={day.id}>
-                    {(alarmDays.length === 1) ||
-                    (alarmDays.length === 5 && alarmDays.every(day => weekdays.includes(day))) ||
-                    (alarmDays.length === 2 && alarmDays.every(day => weekends.includes(day))) ||
-                    (alarmDays.length === 7) ||
+                    {oneDay ||
+                    everyWeekday ||
+                    everyWeekend ||
+                    everyDay ||
                     (day.short)}
                 </div>
             ))}
