80793ad631c5c01a0c8ce0dc8a3c0e87b78b4a83
Christopher Chueng <christopherchueng@gmail.com>
2023-04-04 12:17:08 -0400

    useMemo on ErrorMessage

--- react-app/src/components/ErrorMessage/ErrorMessage.js
+++ react-app/src/components/ErrorMessage/ErrorMessage.js
@@ -1,9 +1,13 @@
+import { useMemo } from 'react'
+
 const ErrorMessage = ({ error, setClassName }) => {
-    return (
+    const errorMsg = useMemo(() => (
         <p className={setClassName}>
             {error ? error : undefined}
         </p>
-    )
+    ), [error, setClassName])
+
+    return errorMsg
   }
 
   export default ErrorMessage;
